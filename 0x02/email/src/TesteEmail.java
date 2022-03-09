fdb2a72e4bb2eb32908ad0797c56cdcd062a4c40
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-03-09 10:06:17 -0300

    Task 00

--- 0x02/email/src/TesteEmail.java
+++ 0x02/email/src/TesteEmail.java
@@ -1,13 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TesteEmail {
-    public static boolean testar_email_com_arroba() {
-        return Pessoa.emailValid("email_teste@dominio.com.br");
+
+    @Test
+    void testar_email_com_arroba() {
+        boolean email = Pessoa.emailValid("email_teste@dominio.com.br");
+        assertTrue(email);
     }
 
-    public static boolean testar_email_sem_arroba() {
-        return Pessoa.emailValid("email_testedominio.com.br");
+    @Test
+    void testar_email_sem_arroba() {
+        boolean email = Pessoa.emailValid("email_testedominio.com.br");
+        assertTrue(email);
     }
 
-    public static boolean testar_email_mais_50_caracteres() {
-        return Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br");
+    @Test
+    void testar_email_mais_50_caracteres() {
+        boolean email = Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br");
+        assertEquals(email, false);
     }
 }
